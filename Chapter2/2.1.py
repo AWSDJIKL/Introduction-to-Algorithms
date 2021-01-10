308a1fd87d9a2ed32e5150bd5b6a55938021b5dc
AWSDJIKL <1164958808@qq.com>
2021-01-10 15:45:47 +0800

    2.1-3 线性查找

--- Chapter2/2.1.py
+++ Chapter2/2.1.py
@@ -13,6 +13,15 @@ def insertion_sort(list, reverse=False):
             result.append(key)
     return result
 
+# 2.1-3 线性查找
+def linear_search(list,v):
+    result = None
+    for i in range(len(list)):
+        if list[i] == v:
+            result = i
+            return result
+    return result
+
 
 test_list = [3, 2, 5, 1, 6]
 print(insertion_sort(test_list, False))
