bd92d8369ae58a8024122cfb928491702b27a1ee
AWSDJIKL <1164958808@qq.com>
2021-01-10 15:39:30 +0800

    添加反序功能

--- Chapter2/2.1.py
+++ Chapter2/2.1.py
@@ -1,9 +1,10 @@
-def insertion_sort(list):
+# 插入排序
+def insertion_sort(list, reverse=False):
     result = [list[0]]
     for i in range(1, len(list)):
         key = list[i]
         for j in range(len(result)):
-            if result[j] < key:
+            if bool((result[j] < key) - reverse):
                 continue
             else:
                 result.insert(j, key)
@@ -14,4 +15,5 @@ def insertion_sort(list):
 
 
 test_list = [3, 2, 5, 1, 6]
-print(insertion_sort(test_list))
+print(insertion_sort(test_list, False))
+print(insertion_sort(test_list, True))
